e734ce78314058cd407c10df3769c541b13dee78
Andy Lui <awlui2014@gmail.com>
2018-01-17 22:37:56 -0800

    Move initial state to constants folder

--- src/guideConstants.js
+++ src/guideConstants.js
@@ -1,6 +1,14 @@
 const cities = [
   'san jose', 'santiago', 'san francisco', 'santa rosa', 'san juan', 'sabadell', 'salamanca', 'salt lake city', 'salinas', 'salem', 'sausalito', 'taipei', 'tel aviv', 'tempe', 'termez', 'temuco', 'tiajuna', 'tieling', 'thousand oaks', 'thunder bay', 'tokyo', 'tulsa'
 ]
+
+const initialState = {
+      cities,
+      currentInput: '',
+      dirty: false
+    }
+
 export {
-  cities as CITIES
+  cities as CITIES,
+  initialState
 }
