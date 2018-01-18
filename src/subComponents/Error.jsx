0d1c0a9af9baee5ecb49907bc64b57819a3a7a5c
Andy Lui <awlui2014@gmail.com>
2018-01-17 23:00:43 -0800

    Fix spelling mistake

--- src/subComponents/Error.jsx
+++ src/subComponents/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Error.css'
 const ErrorMessage = ({dirty, currentInput}) => (
         <div className="errorMessage">
-               <div className={(dirty && (currentInput.length < 3)) ? 'errorActive' : 'errorInActive'}>Valid search requries three letters.</div>
+               <div className={(dirty && (currentInput.length < 3)) ? 'errorActive' : 'errorInActive'}>Valid search requires a minimum of three letters.</div>
         </div>
   );
 
