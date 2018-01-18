e1fd5b8972a575d8d68b9c014cd0a52bd3b54d55
Andy Lui <awlui2014@gmail.com>
2018-01-17 22:36:41 -0800

    Add list components w/ test

--- src/subComponents/List.jsx
+++ src/subComponents/List.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const List = () => (
+const List = ({cities, currentInput, dirty}) => (
   <div className="cityList">
     <h2>City List</h2>
     <ul>
+    { currentInput.length >= 3 ? cities
+      .filter((city) => {
+        return city.startsWith(currentInput);
+      })
+      .map((city, i) => (<li key={i}>{city}</li>)) : null }
     </ul>
   </div>
   );
--- src/subComponents/List.test.jsx
+++ src/subComponents/List.test.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import List from './List';
-
-
+import { initialState } from '../guideConstants';
+ 
 
 describe('The List component', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<List />);
+    wrapper = shallow(<List {...initialState} />); //since the props that the list component takes map one to one to the app state, we can just use the spread operator on the state
   });
   it('Initially has no li elements', () => {
     expect(wrapper.find('li').length).toEqual(0);
