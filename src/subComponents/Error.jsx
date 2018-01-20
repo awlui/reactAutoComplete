4e47be33ffe101145a944916d292305e841e4070
Andy Lui <awlui2014@gmail.com>
2018-01-20 11:11:09 -0800

    Refactor Error component to utilize suggestedCities length

--- src/subComponents/Error.jsx
+++ src/subComponents/Error.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import './Error.css'
-const ErrorMessage = ({dirty, currentInput, cities}) => (
+import './Error.css';
+import PropTypes from 'prop-types';
+
+const ErrorMessage = ({dirty, currentInput, suggestedCities}) => (
        <div className="errorMessage">
              {(dirty && (currentInput.length < 3)) ? <div>Valid search requires a minimum of three letters.</div> : null}
              { 
-              currentInput.length >= 3 
-               &&
-               !cities.some((city) => {
-                 return city.startsWith(currentInput);
-             }) 
+              currentInput.length >= 3 && suggestedCities.length === 0
                ? 
                <div>No Results</div> : null
              }
        </div>
   );
 
+ErrorMessage.propTypes = {
+  dirty: PropTypes.bool,
+  currentInput: PropTypes.string,
+  suggestedCities: PropTypes.array
+}
 export default ErrorMessage;
