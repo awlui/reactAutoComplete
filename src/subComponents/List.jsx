a581b9472cf519b57069019d81341ca9899af6a9
Andy Lui <awlui2014@gmail.com>
2018-01-20 11:13:24 -0800

    Refactor list component to utilize suggestedCities instead of filtering the entire Cities array

--- src/subComponents/List.jsx
+++ src/subComponents/List.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './List.css' ;
+import PropTypes from 'prop-types';
 
-const List = ({cities, currentInput}) => (
+
+const List = ({suggestedCities, currentInput, currentIndex, onListHover, displayList}) => (
   <div className="cityList">
     <ul>
-    { currentInput.length >= 3 ? cities
-      .filter((city) => {
-        return city.startsWith(currentInput);
-      })
-      .map((city, i) => (<li key={i}><strong>{city.substr(0, currentInput.length)}</strong>{city.substr(currentInput.length)}</li>)) : null }
+    { displayList && currentInput.length >= 3 ? suggestedCities
+      .map((city, i) => (<li onMouseMove={onListHover.bind(this, i)} className={Number.isInteger(currentIndex) && currentIndex === i ? 'activeCity' : null } key={i}><strong>{city.substr(0, currentInput.length)}</strong>{city.substr(currentInput.length)}</li>)) : null }
     </ul>
   </div>
   );
+
+List.propTypes = {
+  displayList: PropTypes.bool,
+  currentInput: PropTypes.string,
+  currentIndex: PropTypes.number,
+  onListHover: PropTypes.func,
+  suggestedCities: PropTypes.array
+}
 
 export default List;
