7b6918e39b60d7d910a057a32fa4eb2f7252a4a0
Andy Lui <awlui2014@gmail.com>
2018-01-20 11:15:14 -0800

    Integrate methods for up/down arrow feature

--- src/subComponents/Search.jsx
+++ src/subComponents/Search.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Search.css';
-const Search = ({onSearchInput, refCallback}) => (
+const Search = ({onSearchInput, refCallback, onArrowPress, onFocusOut, currentInput, currentIndex, suggestedCities, onEnter}) => (
   <form onSubmit={(event) => event.preventDefault() }>
-    <input ref={refCallback} type="text" className="searchField" onChange={onSearchInput}/>
+    <input ref={refCallback} type="text" value={Number.isInteger(currentIndex) ?  suggestedCities[currentIndex] : currentInput} className="searchField" onChange={onSearchInput} onKeyDown={onArrowPress} onBlur={onFocusOut}
+
+    />
   </form>
   );
 
+Search.propTypes = {
+  suggestedCities: PropTypes.array,
+  currentInput: PropTypes.string,
+  onSearchInput: PropTypes.func,
+  refCallback: PropTypes.func,
+  onArrowPress: PropTypes.func,
+  onFocusOut: PropTypes.func,
+  currentIndex: PropTypes.number,
+  onEnter: PropTypes.func
+}
 export default Search;
